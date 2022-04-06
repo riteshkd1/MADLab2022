fee26e3396198fbb4685463d2b92f5913582ec79
ritesh <riteshkd997@gmail.com>
2022-04-06 22:00:43 +0545

    HelloToastChallenge2

--- app/src/main/java/com/example/madlab2022/MainActivity.java
+++ app/src/main/java/com/example/madlab2022/MainActivity.java
@@ -7,7 +7,15 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * Displays two Buttons and a TextView.
+ * - Pressing the TOAST button shows a Toast.
+ * - Pressing the COUNT button increases the displayed mCount.
+ *
+ */
+
 public class MainActivity extends AppCompatActivity {
+
     private int mCount = 0;
     private TextView mShowCount;
 
@@ -17,12 +25,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
     }
+
+    /*
+     * Shows a Toast when the TOAST button is clicked.
+     *
+     * @param view The view that triggered this onClick handler.
+     *             Since a toast always shows on the top,
+     *             the passed in view is not used.
+     */
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_message,
                 Toast.LENGTH_SHORT);
         toast.show();
     }
 
+    /*
+     * Increments the number in the TextView when the COUNT button is clicked.
+     *
+     * @param view The view that triggered this onClick handler.
+     *             Since the count always appears in the TextView,
+     *             the passed in view is not used.
+     */
     public void countUp(View view) {
         mCount++;
         if (mShowCount != null)
